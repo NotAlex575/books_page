405296e1235df0758958e28fe06ec2426899b93b
NotAlex575 <dapontealessandro89gmail.com>
2025-09-24 20:36:16 +0200

    aggiunta description nella card singola

--- src/components/Bookcard_selected.jsx
+++ src/components/Bookcard_selected.jsx
@@ -1,5 +1,5 @@
 const Bookcard_selected = ({ book }) => {
-  const {title, image, author } = book
+  const {title, image, author, abstract} = book
     return (
         <div className="col-12">
             <div className="card medium-card">
@@ -11,6 +11,7 @@ const Bookcard_selected = ({ book }) => {
                 <div className="overlay">
                   <h2 className="text-center my-3">{title}</h2>
                   <p className="text-center">{author}</p>
+                  <p className="text-center">{abstract}</p>
                 </div>
             </div>
         </div>
